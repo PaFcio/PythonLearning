95cdea7f66853cd9b44b3aabee1c5134ea63a398
PaFcio <pafelcio@gmail.com>
2021-11-06 10:15:02 +0100

    Some types and things connected with those

---
+++ TypeConversions.py
@@ -0,0 +1,30 @@
+# Integer
+i = 2
+print(type(i))
+
+# Floating point
+f = 2.71828
+print(type(f))
+
+# Complex no.
+c = 0.5 + 1j
+print(type(c))
+
+# Type conversion
+print(float(i), '&', int(f), '&', c)
+
+# Boolean
+a = 2 > 1
+print(a)
+print(type(a))
+
+# String
+znak = 'A'
+print(znak)
+print(type(znak))
+
+napis = "Ala ma kota"
+print(napis[0])
+
+# HERE IMPORTANT, HAS NOT ONLY START & STOP BUT ALSO STEP!!!
+print(napis[0::2])
