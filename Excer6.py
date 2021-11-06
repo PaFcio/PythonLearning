3ab87f0c46f926d8a9298cb4774bffe65a6cec2b
PaFcio <pafelcio@gmail.com>
2021-11-06 12:49:28 +0100

    Excercise 6

--- Excer6.py
+++ Excer6.py
@@ -10,6 +10,6 @@ l1 = float(input('Podaj liczbę 1: '))
 # Input na podanie liczby 2
 l2 = float(input('Podaj liczbę 2: '))
 
-print('Wynik dzielenia to: ' + str(l1/l2))
-print('Reszta z dzielenia całkowitego: ' + str(l1%l2))
-print('Dzielenie całkowite :' + str(l1//l2))
+print('Wynik dzielenia to: ' + str(l1 / l2))
+print('Reszta z dzielenia całkowitego: ' + str(l1 % l2))
+print('Dzielenie całkowite :' + str(l1 // l2))
