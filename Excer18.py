02b17ed10e29f4cd736a206ba79ba3d70155ebec
PaFcio <pafelcio@gmail.com>
2021-11-06 14:43:38 +0100

    Excercise 18

---
+++ Excer18.py
@@ -0,0 +1,8 @@
+# Liczby pierwsze - przykład na break
+
+for n in range(2, 100):
+    for x in range(2, n):
+        if n % x == 0:
+            break
+    else:
+        print(n, 'jest liczbą pierwszą')
