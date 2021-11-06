19deebf2b92bc07da7d08b50a1f26cf944fe0811
PaFcio <pafelcio@gmail.com>
2021-11-06 15:35:31 +0100

    Excercise 9 - przykład na and i zwracanie true poprzez funkcję

--- Excer9.py
+++ Excer9.py
@@ -1,6 +1,5 @@
-# Przykład – and
-# Zwróć True, jeśli oba stwierdzenia są prawdziwe
-# x > 3 i x < 10
+# Przykład na and (bez and bo zwarte zamiast x > 3 and x < 10 do 3 < x < 10)
+# Zwróć True, jeśli oba stwierdzenia są prawdziwe x > 3 i x < 10
 
 x = int(input('Podaj x: '))
 
