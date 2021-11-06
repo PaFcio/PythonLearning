b074ff03a5dfd8fe002a5155158bb2ccc47ba0d1
PaFcio <pafelcio@gmail.com>
2021-11-06 10:57:26 +0100

    Excercise 1

--- Excer1.py
+++ Excer1.py
@@ -1,3 +1,5 @@
+# Biorąc pod uwagę ciąg o nieparzystej długości większej niż 7,
+# zwróć łańcuch złożony z trzech środkowych znaków danego ciągu:
 str1 = "Datatypes"
 st_len = len(str1)
 mid_ind = int(st_len/2)
