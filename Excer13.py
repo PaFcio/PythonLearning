3e32a81739d25f3f0d41e97f412f1e8bf23a1acc
PaFcio <pafelcio@gmail.com>
2021-11-06 14:02:53 +0100

    Excercise 13

---
+++ Excer13.py
@@ -0,0 +1,32 @@
+# Zadanie
+# Utwórz listę zawierającą imiona wszystkich kursantów
+# Następnie ją posortuj alfabetycznie, oraz
+# Policz ile z osób na liście jest kobietami
+# W tym celu możesz sprawdzić czy imię kończy się na „a”
+
+kursanci = ['Anastazja',
+            'Tomasz',
+            'Magdalena',
+            'Michał',
+            'Paweł',
+            'Paweł',
+            'Elżbieta',
+            'Joanna',
+            'Michał',
+            'Łukasz',
+            'Kamil',
+            'Marcin',
+            'Jakub',
+            'Natalia']
+
+kursanci.sort()
+count = 0
+
+for kurstant in kursanci:
+    if kurstant[-1] == 'a':
+        print('Kursant ' + kursanci[kursanci.index(kurstant)] + ' jest kobietą')
+        count = count + 1
+    else:
+        print('Kursant ' + kursanci[kursanci.index(kurstant)] + ' jest mężczyzną')
+
+print('Ilość kobiet wśród kursantów to: ' + str(count))
