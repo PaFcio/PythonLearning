233e90158a9727444fd30a6e3b609175b79e82bc
PaFcio <pafelcio@gmail.com>
2021-11-06 11:05:32 +0100

    Excercise 2

--- Excer2.py
+++ Excer2.py
@@ -1,6 +1,6 @@
 # Biorąc pod uwagę 2 ciągi, s1 i s2, utwórz nowy ciąg, dodając s2 w środku s1
 s1 = "FullStack"
 s2 = "Python"
-slen = int(len(s1) / 2)
-s3 = s1[:slen] + s2 + s1[slen:]
+half = int(len(s1) / 2)
+s3 = s1[:half] + s2 + s1[half:]
 print(s3)
