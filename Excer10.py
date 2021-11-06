d6f70de89a1fa6de17040bd8c2a229cb5e387bde
PaFcio <pafelcio@gmail.com>
2021-11-06 15:36:15 +0100

    Excercise 10 - przykład na or

--- Excer10.py
+++ Excer10.py
@@ -1,7 +1,5 @@
-# Przykład – or
-# Zwróć wartość True, jeśli jedno ze stwierdzeń jest prawdziwe
-# x > 4 lub x < 3
-# Spróbuj!
+# Przykład na or
+# Zwróć wartość True, jeśli jedno ze stwierdzeń jest prawdziwe x > 4 lub x < 3
 
 x = int(input('Podaj x: '))
 print(x > 4 or x < 3)
