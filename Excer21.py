24cd9e8bb87c6774e03b60caa7014775a6bacb38
PaFcio <pafelcio@gmail.com>
2021-11-06 15:04:06 +0100

    Excercise 21 - przykład na pass (placeholder przy developemencie)

---
+++ Excer21.py
@@ -0,0 +1,9 @@
+# Przykład na pass (placeholder przy developemencie)
+
+name = input("Proszę wpisać swoje imię.")
+# Wpisz swoją odpowiedź tutaj.
+
+if len(name) > 0:
+    print(name)
+else:
+    pass
