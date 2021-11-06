0533a0ce160f06ced284de73de77da6e6bf31e1b
PaFcio <pafelcio@gmail.com>
2021-11-06 15:36:58 +0100

    Excercise 11 - przykład na not

--- Excer11.py
+++ Excer11.py
@@ -1,7 +1,5 @@
-# Przykład – not
-# Odwróć wynik, zwróć False, jeśli wynik jest prawdziwy
-# nie ( x > 3 i x < 10 )
-# Spróbuj!
+# Przykład na not
+# Odwróć wynik, zwróć False, jeśli wynik jest prawdziwy 'nie ( x > 3 i x < 10 )'
 
 x = int(input('Podaj x: '))
 print(not(3 < x < 10))
