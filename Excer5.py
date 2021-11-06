8396d96a38e73c73c1f1a45b6b3406c700a59d01
PaFcio <pafelcio@gmail.com>
2021-11-06 11:45:06 +0100

    Excercise 5

---
+++ Excer5.py
@@ -0,0 +1,26 @@
+# Utwórz listę zawierającą imiona wszystkich kursantów
+# Następnie ją posortuj alfabetycznie, oraz
+# Sprawdź ile elementów ona zawiera
+
+kursanci = ['Anastazja',
+            'Tomasz',
+            'Magdalena',
+            'Michał',
+            'Paweł',
+            'Paweł',
+            'Elżbieta',
+            'Joanna',
+            'Michał',
+            'Łukasz',
+            'Kamil',
+            'Marcin',
+            'Jakub',
+            'Natalia']
+
+kursanci.sort()
+print('Kurs ma ' + str(len(kursanci)) + ' kursantów i są to: ' + str(kursanci))
+
+kursanci.reverse()
+
+kursanci_sorted = sorted(kursanci)
+print('Kurs ma ' + str(len(kursanci)) + ' kursantów i są to: ' + str(kursanci_sorted))
