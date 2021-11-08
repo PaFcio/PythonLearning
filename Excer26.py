d1abb54c4d161183f2527fe734b1e08d2aeb10f9
PaFcio <pafelcio@gmail.com>
2021-11-08 19:20:36 +0100

    Excercise 26 - zsumuj el na liście

---
+++ Excer26.py
@@ -0,0 +1,17 @@
+# Napisz funkcję w Pythonie, który zsumuje wszystkie elementy na liście.
+
+def sum_list(my_list):
+    sum = 0
+    for el in my_list:
+        sum += el
+    return sum
+
+
+in_list = []
+
+val = input('Podaj element do listy: ')
+while val != '':
+    in_list.append(int(val))
+    val = input('Podaj element do listy: ')
+
+print(sum_list(in_list))
