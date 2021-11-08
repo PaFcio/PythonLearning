933fa0cad04f1a5f9ada1b4a4e7b2e41b060d8a4
PaFcio <pafelcio@gmail.com>
2021-11-08 19:40:56 +0100

    Excercise 28 - zwróć max wartość z listy

---
+++ Excer28.py
@@ -0,0 +1,18 @@
+# Napisz funkcję w Pythonie, aby uzyskać największą liczbę z listy.
+
+def max_in_list(my_list):
+    max_val = my_list[0]
+    for el in my_list:
+        if el > max_val:
+            max_val = el
+    return max_val
+
+
+in_list = []
+val = input('Podaj element do listy: ')
+
+while val != '':
+    in_list.append(int(val))
+    val = input('Podaj element do listy: ')
+
+print(max_in_list(in_list))
