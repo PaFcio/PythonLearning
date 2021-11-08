96eb2f2fa8337a23cd793a15e03a5cdc6811b33c
PaFcio <pafelcio@gmail.com>
2021-11-08 21:05:44 +0100

    Excercise 33 - policz silnię

---
+++ Excer33.py
@@ -0,0 +1,13 @@
+# Napisz program, policzy silnię dla liczby n tj.
+# n! = 1*2*3*4...*(n-2)*(n-1)    *n
+# Zrób to przez napisanie funkcji, która rekurencyjne będzie się odwoływała do samej siebie
+# do momentu gdy będzie liczyła silnie dla 1, która wynosi 1.
+
+def silnia(n):
+    if n > 1:
+        return silnia(n -1) * n
+    else:
+        return 1
+
+
+print(silnia(2))
