be1c8f3ed82253a0333761964833f4e469773cfa
PaFcio <pafelcio@gmail.com>
2021-11-15 19:41:47 +0100

    Excercise 45 - wyjątek na File not found

---
+++ Excer45.py
@@ -0,0 +1,17 @@
+# Spróbuj otworzyć do czytania plik (podpowiedź: f = open(arg, "r")).
+# W razie braku możliwości otwarcia pliku, obsłuż wyjątek.
+# W przeciwnym przypadku wypisz:
+# Nazwę pliku;
+# Liczbę wierszy (podpowiedź: len(f.readlines()).
+# Na koniec zamknij ten plik (podpowiedź: f.close()).
+
+# arg = "readme2.txt"
+arg = "Excer7.py"
+
+try:
+    f = open(arg, "r")
+except FileNotFoundError:
+    print("File not found! Filename: " + arg)
+else:
+    print("Plik " + arg + " ma " + str(len(f.readlines())) + " wierszy")
+    f.close()
